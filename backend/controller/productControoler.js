945998e0f665ad75d3d4e8d1c5774a638010dc40
Vinod samy <vino929510@gmail.com>
2020-11-24 10:46:06 +0530

    [ok]add deleteProduct function backend

--- backend/controller/productControoler.js
+++ backend/controller/productControoler.js
@@ -17,4 +17,15 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProductById, getProducts }
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    await product.remove()
+    res.json({ message: "Product removed." })
+  } else {
+    res.status(404)
+    throw new Error("Product not found")
+  }
+})
+
+export { getProductById, getProducts, deleteProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
 import express from "express"
 const router = express.Router()
-import { getProducts, getProductById } from "../controller/productControoler.js"
-
+import { getProducts, getProductById, deleteProduct } from "../controller/productControoler.js"
+import { protect, isAdmin } from "../middleware/authMiddleware.js"
 router.route("/").get(getProducts)
-router.route("/:id").get(getProductById)
+router.route("/:id").get(getProductById).delete(protect, isAdmin, deleteProduct)
 export default router
