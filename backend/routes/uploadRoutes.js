66cf384819f84687cae3dea8bb33ce2fb77d331c
Vinod samy <vino929510@gmail.com>
2020-11-26 11:15:09 +0530

    [ok]setup the multer on backend

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,37 @@
+import express from "express"
+import multer from "multer"
+import path from "path"
+const router = express.Router()
+
+const stroage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "/uploads")
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+  },
+})
+
+function checkFileType(file, cb) {
+  const fileTypes = "/jpg|jpeg|png/"
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+  const mime = fileTypes.test(file.mimetype)
+  if (extname && mime) {
+    return cb(null, true)
+  } else {
+    cb("Image only allowed!")
+  }
+}
+
+const upload = multer({
+  stroage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb)
+  },
+})
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`)
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express"
+import path from "path"
 import connectDB from "./config/db.js"
 import dotenv from "dotenv"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorHandlerMiddleware.js"
 import { protect } from "./middleware/authMiddleware.js"
 const app = express()
@@ -19,11 +21,12 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
-
+app.use("/api/upload", uploadRoutes)
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 app.use(notFound)
 app.use(errorHandler)
 app.use(protect)
-
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 app.listen(5000, console.log(`server running in ${process.env.NODE_MODE} on port ${process.env.PORT}`))
