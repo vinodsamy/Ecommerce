ee1ed1102cb31e1e9dfe4898feec26d857ab128b
Vinod samy <vino929510@gmail.com>
2020-11-27 15:02:15 +0530

    [ok]updateOrderToDeliver controller on backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
 const router = express.Router()
-import { addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToPaid } from "../controller/orderController.js"
+import { addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToDeliver, updateOrderToPaid } from "../controller/orderController.js"
 import { isAdmin, protect } from "../middleware/authMiddleware.js"
 
 router.route("/").post(protect, addOrderItems).get(protect, isAdmin, getOrders)
 router.route("/myorders").get(protect, getMyOrders)
 router.route("/:id").get(protect, getOrderById)
 router.route("/:id/pay").put(protect, updateOrderToPaid)
+router.route("/:id/deliver").put(protect, isAdmin, updateOrderToDeliver)
+
 export default router
